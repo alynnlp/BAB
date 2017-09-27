efdc6e8ec0a789aa2ff51c80da4c05bdb6ab6d92
Aileen Pang <aileenpang63@gmail.com>
2017-09-27 17:55:56 -0400

    edit route framework

--- server.js
+++ server.js
@@ -38,10 +38,82 @@ app.use(express.static("public"));
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
 
+//**********************************************GET******************************************************//
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+//users own page
+app.get("/users/:userid",(req, res)=>{
+  res.render("");
+})
+
+//account settings
+app.get("/users/:userid/settings", (req,res)=>{
+  res.render("");
+})
+
+//Discover
+app.get("/discover", (req,res)=>{
+  res.render("");
+})
+
+//create - categorizing saved pins
+app.get("/users/:userid/:topic", (req,res)=>{
+  res.render("");
+})
+
+//Resource
+app.get("/resources/:resourceid",(req,res)=>{
+  res.render("");
+})
+
+
+//**********************************************POST******************************************************//
+
+
+//Home - Logged In
+app.post("/", (req, res)=>{
+  res.redirect("");
+})
+
+//Delete - users pins/ownpage
+// /users/:userid/ - just user pins (GET, DELETE)
+app.post("/users/:userid", (req,res)=>{
+  res.redirect("");
+})
+
+//Update(PUT) users existing information
+app.post("/users/:userid/settings", (req,res)=>{
+  res.redirect("");
+})
+
+//Register
+app.post("/register", (req,res)=>{
+  res.redirect("");
+})
+
+//Login
+app.post("/login",(req,res)=>{
+  res.redirect("");
+})
+
+//Logout
+app.post("/logout",(req,res)=>{
+  res.redirect("");
+})
+
+//Resource - updating comment
+app.post("urls/", (req,res)=>{
+  res.redirect("");
+})
+
+//Resource - delete comment
+app.post("/resources/:resourceid", (req,res)=>{
+  res.redirect("");
+})
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
