e8ad45a2089cc1357ba7fa9b33682a1f8ea03077
Bryce <brycewalterparker@gmail.com>
2017-09-27 23:26:01 +0000

    added all seed files

--- db/seeds/resources.js
+++ db/seeds/resources.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('resources').del()
     .then(function () {
       return Promise.all([
-        knex('resources').insert({title: faker.lorem.words(), description: faker.lorem.sentences(), URL: faker.internet.domainName()}),
-        knex('resources').insert({title: faker.lorem.words(), description: faker.lorem.sentences(), URL: faker.internet.domainName()}),
-        knex('resources').insert({title: faker.lorem.words(), description: faker.lorem.sentences(), URL: faker.internet.domainName()})
+        knex('resources').insert({id: 1, title: faker.lorem.words(), description: faker.lorem.sentences(), URL: faker.internet.domainName()}),
+        knex('resources').insert({id: 2, title: faker.lorem.words(), description: faker.lorem.sentences(), URL: faker.internet.domainName()}),
+        knex('resources').insert({id: 3, title: faker.lorem.words(), description: faker.lorem.sentences(), URL: faker.internet.domainName()})
       ]);
     });
 };
