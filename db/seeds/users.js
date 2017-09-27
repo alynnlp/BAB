9e4a6fdaa0e140479784a98631c3584a1c0602af
Bryce <brycewalterparker@gmail.com>
2017-09-27 18:55:02 +0000

    updated users table schema as a group, added faker dependancy

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,10 +1,12 @@
+const faker = require('faker')
+
 exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, first_name: faker.name.firstName(), last_name: faker.name.lastName(), username: faker.internet.userName(), password: faker.internet.password()}),
+        knex('users').insert({id: 2, first_name: faker.name.firstName(), last_name: faker.name.lastName(), username: faker.internet.userName(), password: faker.internet.password()}),
+        knex('users').insert({id: 3, first_name: faker.name.firstName(), last_name: faker.name.lastName(), username: faker.internet.userName(), password: faker.internet.password()})
       ]);
     });
 };
