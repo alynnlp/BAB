1c5044f6c2b95d11aae02fadebffd1b6b79f2a0f
Bryce <brycewalterparker@gmail.com>
2017-09-28 21:29:48 +0000

    updated seeds/added timestamps/fixed case issue/added new queries

--- public/scripts/userqueries.js
+++ public/scripts/userqueries.js
@@ -17,11 +17,11 @@ const faker = require('faker')
 
 //############### USER FUNCTIONS
 
-var userPostResource = function(title, description, URL, user_id, topic_id ) {
+var userPostResource = function(title, description, url, user_id, topic_id ) {
   knex('resources').insert({
     title: title,
     description: description,
-    URL: URL,
+    url: url,
     user_id: user_id,
     topic_id: topic_id
   })
@@ -36,6 +36,7 @@ var userPostResource = function(title, description, URL, user_id, topic_id ) {
 }
 
 // userPostResource('lorem', 'i might be losing my MIND', 'www.nonono.com', 2, 2)
+// userPostResource('lorem', 'what do u mean', 'www.jbieber.com', 3, 4)
 
 var createNewUser = function(first_name, last_name, email, username, password, avatar) {
   return knex('users').insert({
@@ -51,7 +52,7 @@ var createNewUser = function(first_name, last_name, email, username, password, a
 // createNewUser('John', 'Smith', 'johnnyappleseed', 'y28736jhdfshgfo87dfi', faker.image.avatar())
 
 var userLogin = function(username, encryptedPassword) {
-  var promise = new Promise(function(resolve, reject) {
+  let promise = new Promise(function(resolve, reject) {
     knex('users').where({
       username: username,
       password: encryptedPassword
@@ -71,17 +72,68 @@ var userLogin = function(username, encryptedPassword) {
   return promise;
 }
 
-userLogin('Tyshawn_Green18', 'IYqw92tcwrKgoVS')
-  .then(function(res) {
-    console.log(res);
-    knex.destroy()
-  }).catch(function(err) {
-    console.error(err);
-    knex.destroy()
-  });
+// userLogin('Tyshawn_Green18', 'IYqw92tcwrKgoVS')
+//   .then(function(res) {
+//     console.log(res);
+//     knex.destroy()
+//   }).catch(function(err) {
+//     console.error(err);
+//     knex.destroy()
+//   });
 
+var userCreatedResources = function(user_id) {
+  return knex('resources')
+      .join('users', 'resources.user_id', '=', `users.id`)
+      .where('users.id', '=', `${user_id}`)
+      .select('resources.id', 'title', 'description', 'url')
+    .then (result => {
+      if (result.length === 0) {
+        console.log('No resources for this user!')
+      }
+      else {
+        return result
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
 
+// userCreatedResources(4)
+//   .then(function(res) {
+//     console.log(res)
+//     knex.destroy()
+//   })
+//   .catch(function(err) {
+//     console.error(err);
+//     knex.destroy()
+//   });
 
+var getResourcesByTopic = function(topic_id) {
+  return knex('resources')
+      .join('topics', 'resources.topic_id', '=', `topics.id`)
+      .where('topics.id', '=', `${topic_id}`)
+      .select('resources.id', 'title', 'description', 'url')
+    .then (result => {
+      if (result.length === 0) {
+        console.log('No resources for this topic!')
+      }
+      else {
+        return result
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
 
-
+// getResourcesByTopic(4)
+//   .then(function(res) {
+//     console.log(res)
+//     knex.destroy()
+//   })
+//   .catch(function(err) {
+//     console.error(err);
+//     knex.destroy()
+//   });
 
