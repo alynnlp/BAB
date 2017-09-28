597873f9a41b60f64fee537bdc470da5d3abb5d4
Aileen Pang <aileenpang63@gmail.com>
2017-09-28 18:07:40 -0400

    icon button, setting button and rendering new cards function edited

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,5 @@
-function createNewResources(cardObject) {
-  //$('') > going to search for the tag name in html; $("< >") > going to add an element TAG
-    var $card = $('<div>').addClass('card');
-    var $imgWrapper = $('<div>').addClass('pin-image-wrapper');
-    //tweetobject.user.avatars.small - place holder
-    var $img = $('<img src="../../images/architecture.jpg"/>').addClass('card-img-top');
-    var $imgOverlay = $('<div>').addClass('card-img-overlay');
-    //cardtitle place holder
-    var $cardTitle = $('<h4>').addClass('card-title').text(card-title);
-    var $cardBody = $('<div>').addClass('card-body');
-    var $cardText = $('<p>').addClass('card-text').text(At est quidam suavitate, error delicatissimi cum no. Nam falli dictas maluisset ea, te laudem iracundia usu. Pro elit albucius ei, ex inani repudiandae usu. Modus audiam scribentur in eos, in sit purto gloriatur, saperet meliore ius te. Democritum voluptaria ne vim, eos mundi apeirian conclusionemque ex, ea qui duis option temporibus.);
-    var $cardFooter = $('<div>').addClass('card-footer');
-    var $textMuted = $('<small>').addClass('text-muted').text(Last updated 3 mins ago);
-    var $timePosted = cardObject[]
-
+$(document).ready(function() {
+//timeStamp converter
   function convertDate(dateNow, dateCreated) {
     let timeAmount = dateNow - dateCreated;
    // Less than a minute
@@ -47,9 +34,66 @@ function createNewResources(cardObject) {
       return “Over a year ago”;
     }
   }
-convertDate(Date.now(),timePosted);
 
-$(() => {
+//1. JS function for Saving New Card on Users Page
+  function createNewCard(cardObject) {
+  //$('') > going to search for the tag name in html; $("< >") > going to add an element TAG
+    var $card = $('<div>').addClass('card');
+    var $icon = $('<div>').addClass('icon');
+    var $imgWrapper = $('<div>').addClass('pin-image-wrapper');
+    //tweetobject.user.avatars.small - place holder
+    var $img = $('<img src="../../images/architecture.jpg"/>').addClass('card-img-top');
+    var $imgOverlay = $('<div>').addClass('card-img-overlay');
+    //cardtitle place holder
+    var $cardTitle = $('<h4>').addClass('card-title').text(card-title);
+    var $cardBody = $('<div>').addClass('card-body');
+    var $cardText = $('<p>').addClass('card-text').text(At est quidam suavitate, error delicatissimi cum no. Nam falli dictas maluisset ea, te laudem iracundia usu. Pro elit albucius ei, ex inani repudiandae usu. Modus audiam scribentur in eos, in sit purto gloriatur, saperet meliore ius te. Democritum voluptaria ne vim, eos mundi apeirian conclusionemque ex, ea qui duis option temporibus.);
+    var $cardFooter = $('<div>').addClass('card-footer');
+
+    var timeConverted = convertDate(Date.now(),timePosted);
+    var $textMuted = $('<small>').addClass('text-muted').text(`Last updated ${timeConverted} mins ago`);
+
+    var $pIcon = $('<p>').addClass('icon');
+        var iconsArray = ['fa-bookmark-o','fa-heart','fa-star'];
+        iconsArray.map(function(icon) {
+          var iconList = '<i class="fa' + ' ' + icon + '"></i>';
+          $pIcon.append(iconList);
+        });
+    //jQuery to put the Tag together
+    $card.append($icon);
+    $card.append($imgWrapper);
+    $imgWrapper.append($img);
+    $imgWrapper.append($imgOverlay);
+    $imgOverlay.append($cardTitle);
+    $card.append($cardBody);
+    $cardBody.append($cardText);
+    $card.append($cardFooter);
+    $cardFooter.append($textMuted);
+    return $card;
+}
+//function to prepend the new card on top of UsersPage
+function renderCard(cardArray){
+  cardArray.forEach(function(card){
+    var $newcard = createNewCard(card)
+    $('container card-columns').prepend($newcard)
+  });
+}
+function loadCard(){
+  $('container card-columns').empty();
+  $.ajax({
+    method:"GET",
+    url:"/user",
+    success: function (arrayOfCards){
+      console.log('Success': arrayOfCards)
+      renderCard(arrayOfCards)
+    },
+  });
+}
+
+loadCard();
+
+
+//get users
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -58,4 +102,8 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
-});
+
+
+
+//
+})
