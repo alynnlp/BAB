aeedf057e5a4a27e4c79585ba7d8892f288e9d0a
Bryce <brycewalterparker@gmail.com>
2017-09-28 19:21:21 +0000

    updated seeds/schema/userLogin function implemented

--- public/scripts/userqueries.js
+++ public/scripts/userqueries.js
@@ -15,6 +15,8 @@ const faker = require('faker')
 
 //############### EXPORT FUNCTIONS
 
+//############### USER FUNCTIONS
+
 var userPostResource = function(title, description, URL, user_id, topic_id ) {
   knex('resources').insert({
     title: title,
@@ -24,7 +26,7 @@ var userPostResource = function(title, description, URL, user_id, topic_id ) {
     topic_id: topic_id
   })
   .then(result => {
-    console.log('success')
+    console.log('Resource posted!')
   })
   .catch(err => {
     throw (err)
@@ -33,13 +35,50 @@ var userPostResource = function(title, description, URL, user_id, topic_id ) {
   knex.destroy()
 }
 
-userPostResource('lorem', 'i might be losing my MIND', 'www.nonono.com', 1, 2)
+// userPostResource('lorem', 'i might be losing my MIND', 'www.nonono.com', 2, 2)
 
-var createNewUser = function(first_name, last_name, username, password, avatar) {
-
+var createNewUser = function(first_name, last_name, email, username, password, avatar) {
+  return knex('users').insert({
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    username: username,
+    password: password,
+    avatar: avatar
+  })
 }
 
+// createNewUser('John', 'Smith', 'johnnyappleseed', 'y28736jhdfshgfo87dfi', faker.image.avatar())
 
+var userLogin = function(username, encryptedPassword) {
+  var promise = new Promise(function(resolve, reject) {
+    knex('users').where({
+      username: username,
+      password: encryptedPassword
+    })
+    .then (result => {
+      if (result.length === 0) {
+        resolve(false);
+      }
+      else {
+        resolve(true);
+      }
+    })
+    .catch(err => {
+      reject(err);
+    });
+  });
+  return promise;
+}
+
+userLogin('Tyshawn_Green18', 'IYqw92tcwrKgoVS')
+  .then(function(res) {
+    console.log(res);
+    knex.destroy()
+  }).catch(function(err) {
+    console.error(err);
+    knex.destroy()
+  });
 
 
 
