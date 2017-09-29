68c2b9a19326c3f32f53c7dd715d87f5a702ccc1
Bryce <brycewalterparker@gmail.com>
2017-09-29 00:06:32 +0000

    ACTUALLY added schema/seed fixes

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, first_name: faker.name.firstName(), last_name: faker.name.lastName(), username: faker.internet.userName(), password: faker.internet.password()}),
-        knex('users').insert({id: 2, first_name: faker.name.firstName(), last_name: faker.name.lastName(), username: faker.internet.userName(), password: faker.internet.password()}),
-        knex('users').insert({id: 3, first_name: faker.name.firstName(), last_name: faker.name.lastName(), username: faker.internet.userName(), password: faker.internet.password()})
+        knex('users').insert({first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), username: faker.internet.userName(), password: faker.internet.password(), avatar: faker.image.avatar()}),
+        knex('users').insert({first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), username: faker.internet.userName(), password: faker.internet.password(), avatar: faker.image.avatar()}),
+        knex('users').insert({first_name: faker.name.firstName(), last_name: faker.name.lastName(), email: faker.internet.email(), username: faker.internet.userName(), password: faker.internet.password(), avatar: faker.image.avatar()})
       ]);
     });
 };
