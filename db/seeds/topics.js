b247e9a6535a19c828e230632d03fca0ddcd87b0
Bryce <brycewalterparker@gmail.com>
2017-09-29 15:56:58 +0000

    updated topics/finished query scripts

--- db/seeds/topics.js
+++ db/seeds/topics.js
@@ -6,12 +6,12 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('topics').insert({topic_name: 'art'}),
         knex('topics').insert({topic_name: 'geography'}),
-        knex('topics').insert({topic_name: 'health'})
-        knex('topics').insert({topic_name: 'literature'})
-        knex('topics').insert({topic_name: 'politics'})
-        knex('topics').insert({topic_name: 'music'})
-        knex('topics').insert({topic_name: 'history'})
-        knex('topics').insert({topic_name: 'math'})
+        knex('topics').insert({topic_name: 'health'}),
+        knex('topics').insert({topic_name: 'literature'}),
+        knex('topics').insert({topic_name: 'politics'}),
+        knex('topics').insert({topic_name: 'music'}),
+        knex('topics').insert({topic_name: 'history'}),
+        knex('topics').insert({topic_name: 'math'}),
         knex('topics').insert({topic_name: 'science'})
       ]);
     });
